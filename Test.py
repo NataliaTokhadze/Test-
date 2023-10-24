26e0a3bf6f281b622fdba83e6f154e7669885b96
Natalia <147244357+NataliaTokhadze@users.noreply.github.com>
2023-10-25 02:47:47 +0400

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,22 @@
+print("Calculate and write the answer:")
+
+print("1) P3 = ?")
+answer_1 = int(input("Enter your answer: "))
+if answer_1 == 6 :
+    print("Right!!!")
+else:
+    print("Wrong...")
+
+print("2) P4 = ?")
+answer_2 = int(input("Enter your answer: "))
+if answer_2 == 24 :
+    print("Right!!!")
+else:
+    print("Wrong...")
+
+print("3) P5 = ?")
+answer_3 = int(input("Enter your answer: "))
+if answer_3 == 120:
+    print("Right!!!")
+else:
+    print("Wrong...")
